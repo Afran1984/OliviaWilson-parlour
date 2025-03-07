8573bd3c70ddf73ef4bc342a8d410c731f80d821
Afran1984 <afranrazzak1984@gmail.com>
2025-03-07 23:22:23 +0600

    update version

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
     }
     
   return (
-    <div className="grid grid-cols-2 bg-gray-100">
+    <div className="grid lg:grid-cols-2 grid-cols-1 bg-gray-100">
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
         <h1 className='text-2xl font-mono'>
@@ -110,3 +110,5 @@ const Login = () => {
 };
 
 export default Login;
+
+
